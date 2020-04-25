4109b6c72eb9d0912e103ec67fae65f52171836c
Alec Maier <Maier.144@osu.edu>
2020-04-25 00:36:15 -0400

    first draft editing of custom roles

--- static/util.js
+++ static/util.js
@@ -19,5 +19,32 @@ export const utility =
             [array[i], array[j]] = [array[j], array[i]];
         }
         return array;
-}
+    },
+
+    validateCustomRolesJsonObject(name, expectedKeys) {
+        let value = localStorage.getItem(name);
+        if (value !== null) {
+            let valueJson;
+            try {
+                valueJson = JSON.parse(value);
+            } catch(e) {
+                console.error(e.message);
+                localStorage.removeItem(name);
+                return false;
+            }
+            if (valueJson && Array.isArray(valueJson)) { // some defensive programming - check if it's an array, and that the object has the expected structure
+                for (let i = 0; i < valueJson.length; i++){
+                    if (expectedKeys.some((key) => !Object.keys(valueJson[i]).includes(key))) {
+                        console.error("tried to read invalid object: " + valueJson[i] + " with expected keys: " + expectedKeys);
+                        valueJson.splice(i, 1);
+                        localStorage.setItem(name, JSON.stringify(valueJson));
+                    }
+                }
+                return valueJson;
+            } else { // object has been messed with. remove it.
+                localStorage.removeItem(name);
+                return false;
+            }
+        }
+    }
 };

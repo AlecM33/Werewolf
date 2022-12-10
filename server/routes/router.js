c6e4486c283f8ca7403b3e65b821a6b271158fe8
AlecM33 <leohfx@gmail.com>
2022-12-10 12:21:03 -0500

    rate limit routes to html pages

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,32 +1,40 @@
 const express = require('express');
 const router = express.Router({ strict: true });
 const path = require('path');
+const rateLimit = require('express-rate-limit').default;
 
-router.get('/', function (request, response) {
+const htmlPageLimiter = rateLimit({
+    windowMs: 60000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false
+});
+
+router.get('/', htmlPageLimiter, function (request, response) {
     response.sendFile(path.join(__dirname, '../../client/src/views/home.html'));
 });
 
-router.get('/create', function (request, response) {
+router.get('/create', htmlPageLimiter, function (request, response) {
     response.sendFile(path.join(__dirname, '../../client/src/views/create.html'));
 });
 
-router.get('/join/:code', function (request, response) {
+router.get('/join/:code', htmlPageLimiter, function (request, response) {
     response.sendFile(path.join(__dirname, '../../client/src/views/join.html'));
 });
 
-router.get('/how-to-use', function (request, response) {
+router.get('/how-to-use', htmlPageLimiter, function (request, response) {
     response.sendFile(path.join(__dirname, '../../client/src/views/how-to-use.html'));
 });
 
-router.get('/game/:code', function (request, response) {
+router.get('/game/:code', htmlPageLimiter, function (request, response) {
     response.sendFile(path.join(__dirname, '../../client/src/views/game.html'));
 });
 
-router.get('/liveness_check', (req, res) => {
+router.get('/liveness_check', htmlPageLimiter, (req, res) => {
     res.sendStatus(200);
 });
 
-router.get('/readiness_check', (req, res) => {
+router.get('/readiness_check', htmlPageLimiter, (req, res) => {
     res.sendStatus(200);
 });
 

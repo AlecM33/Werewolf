249f7c6b74c0c311a9aece93da85869405d76182
AlecM33 <leohfx@gmail.com>
2022-12-21 20:01:24 -0500

    lint

--- spec/unit/server/modules/GameManager_Spec.js
+++ spec/unit/server/modules/GameManager_Spec.js
@@ -6,7 +6,7 @@ const STATUS = globals.STATUS;
 const Person = require('../../../../server/model/Person');
 const GameManager = require('../../../../server/modules/GameManager.js');
 const GameStateCurator = require('../../../../server/modules/GameStateCurator.js');
-const ActiveGameRunner = require("../../../../server/modules/ActiveGameRunner.js");
+const ActiveGameRunner = require('../../../../server/modules/ActiveGameRunner.js');
 const logger = require('../../../../server/modules/Logger.js')(false);
 
 describe('GameManager', () => {

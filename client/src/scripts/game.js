b4ad379414bfc60e09b30b0d3d34c62aaace8c78
AlecM33 <leohfx@gmail.com>
2023-08-13 18:56:04 -0400

    remove redundant promise handler

--- client/src/scripts/game.js
+++ client/src/scripts/game.js
@@ -3,8 +3,6 @@ import { io } from 'socket.io-client';
 import gameTemplate from '../view_templates/GameTemplate.js';
 import { toast } from '../modules/front_end_components/Toast.js';
 
-gameHandler(io('/in-game'), window, gameTemplate).then(() => {
-    toast('Connecting...', 'warning', true, false);
-}).catch((e) => {
+gameHandler(io('/in-game'), window, gameTemplate).catch((e) => {
     toast(e.message, 'error', true, false);
 });

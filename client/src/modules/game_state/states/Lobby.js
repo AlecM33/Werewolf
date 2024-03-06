95f59b6474cb2bb4a4ee935c3a2b4760b0149cf9
AlecM33 <leohfx@gmail.com>
2024-03-06 00:26:28 -0500

    fix import spacing

--- client/src/modules/game_state/states/Lobby.js
+++ client/src/modules/game_state/states/Lobby.js
@@ -1,12 +1,12 @@
-import {QRCode} from '../../third_party/qrcode.js';
-import {toast} from '../../front_end_components/Toast.js';
-import {EVENT_IDS, PRIMITIVES, SOCKET_EVENTS, USER_TYPE_ICONS, USER_TYPES} from '../../../config/globals.js';
-import {HTMLFragments} from '../../front_end_components/HTMLFragments.js';
-import {Confirmation} from '../../front_end_components/Confirmation.js';
-import {SharedStateUtil} from './shared/SharedStateUtil.js';
-import {GameCreationStepManager} from '../../game_creation/GameCreationStepManager.js';
-import {DeckStateManager} from '../../game_creation/DeckStateManager.js';
-import {hiddenMenus} from '../../../view_templates/CreateTemplate.js';
+import { QRCode } from '../../third_party/qrcode.js';
+import { toast } from '../../front_end_components/Toast.js';
+import { EVENT_IDS, PRIMITIVES, SOCKET_EVENTS, USER_TYPE_ICONS, USER_TYPES } from '../../../config/globals.js';
+import { HTMLFragments } from '../../front_end_components/HTMLFragments.js';
+import { Confirmation } from '../../front_end_components/Confirmation.js';
+import { SharedStateUtil } from './shared/SharedStateUtil.js';
+import { GameCreationStepManager } from '../../game_creation/GameCreationStepManager.js';
+import { DeckStateManager } from '../../game_creation/DeckStateManager.js';
+import { hiddenMenus } from '../../../view_templates/CreateTemplate.js';
 
 export class Lobby {
     constructor (containerId, stateBucket, socket) {

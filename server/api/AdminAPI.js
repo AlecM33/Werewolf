a2737f1d8eeea09d8e6ad5389aa33b1b3e793be7
AlecM33 <leohfx@gmail.com>
2025-12-29 14:55:41 -0500

    allow local admin dashboard

--- server/api/AdminAPI.js
+++ server/api/AdminAPI.js
@@ -4,9 +4,9 @@ const debugMode = Array.from(process.argv.map((arg) => arg.trim().toLowerCase())
 const logger = require('../modules/Logger')(debugMode);
 const eventManager = (require('../modules/singletons/EventManager.js')).instance;
 const cors = require('cors');
-const { CORS_OPTIONS, CONTENT_TYPE_VALIDATOR } = require('../config/globals');
+const { ADMIN_CORS_OPTIONS, CONTENT_TYPE_VALIDATOR } = require('../config/globals');
 
-router.use(cors(CORS_OPTIONS));
+router.use(cors(ADMIN_CORS_OPTIONS));
 
 router.post('/sockets/broadcast', (req, res, next) => {
     CONTENT_TYPE_VALIDATOR(req, res, next);
